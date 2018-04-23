dfe1b0b03f2eea7dcb8c3e4aaec543e12aef2d41
Jason Situ <junsitu2@illinois.edu>
2018-04-23 15:36:37 -0500

    fix async read csv file parms

--- src/components/UserStatus/MainSection/CourseVisualize/CourseVisualize.jsx
+++ src/components/UserStatus/MainSection/CourseVisualize/CourseVisualize.jsx
@@ -121,7 +121,7 @@ class CourseVisualize extends Component {
     function readTextFile(file) {
       var ret = {};
       var rawFile = new XMLHttpRequest();
-      rawFile.open("GET", file, true);
+      rawFile.open("GET", file, false);
       rawFile.onreadystatechange = function () {
         if (rawFile.readyState === 4) {
           if (rawFile.status === 200 || rawFile.status == 0) {
